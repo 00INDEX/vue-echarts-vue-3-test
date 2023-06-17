c0e20ec38b8320b7d14eefea976d5f0b2fe3d161
00INDEX <zhangshuolove@live.com>
2023-06-17 16:22:33 +0800

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -5,59 +5,58 @@
 <script setup>
 import { use } from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
-import { PieChart } from 'echarts/charts';
+import { LineChart } from 'echarts/charts';
 import {
   TitleComponent,
   TooltipComponent,
   LegendComponent,
+  GridComponent,
 } from 'echarts/components';
 import VChart, { THEME_KEY } from 'vue-echarts';
 import { ref, provide } from 'vue';
 
 use([
   CanvasRenderer,
-  PieChart,
+  LineChart,
   TitleComponent,
   TooltipComponent,
   LegendComponent,
+  GridComponent,
 ]);
 
-provide(THEME_KEY, 'dark');
+provide(THEME_KEY, 'roma');
 
 const option = ref({
   title: {
-    text: 'Traffic Sources',
-    left: 'center',
+    text: 'Temperature Change in the Coming Week',
   },
   tooltip: {
-    trigger: 'item',
-    formatter: '{a} <br/>{b} : {c} ({d}%)',
+    trigger: 'axis',
   },
-  legend: {
-    orient: 'vertical',
-    left: 'left',
-    data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines'],
+  legend: {},
+  toolbox: {
+    show: true,
+    feature: {
+      dataZoom: {},
+      dataView: { readOnly: false },
+      restore: {},
+      saveAsImage: {},
+    },
+  },
+  xAxis: {
+    type: 'value',
+  },
+  yAxis: {
+    type: 'value',
   },
   series: [
     {
-      name: 'Traffic Sources',
-      type: 'pie',
-      radius: '55%',
-      center: ['50%', '60%'],
+      name: 'Highest',
+      type: 'line',
       data: [
-        { value: 335, name: 'Direct' },
-        { value: 310, name: 'Email' },
-        { value: 234, name: 'Ad Networks' },
-        { value: 135, name: 'Video Ads' },
-        { value: 1548, name: 'Search Engines' },
+        [0, 0],
+        [1, 1],
       ],
-      emphasis: {
-        itemStyle: {
-          shadowBlur: 10,
-          shadowOffsetX: 0,
-          shadowColor: 'rgba(0, 0, 0, 0.5)',
-        },
-      },
     },
   ],
 });
